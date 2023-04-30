2517079ea979d99998a82f2afc712657b2bb7f97
cyril-muzikaloid <cyril.makloufi@gmail.com>
2023-04-30 22:22:05 +0200

    clean

--- cody.py
+++ cody.py
@@ -1,16 +1,15 @@
 import os
-import subprocess
 import openai
-from llama_cpp import Llama
 
-API_KEY = "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"
+def getOpenAIKey():
+     with open("openai.api.key") as file:
+          return file.read()
+
+API_KEY = getOpenAIKey()
 
 prepromptFile = open("preprompt.txt")
 prepromptStr = prepromptFile.read()
 full_conv = prepromptStr
-
-
-llm = Llama(model_path="c:/Users/Cyril/Documents/GPT/LLAMAModel/ggml-model-q4_0.bin")
 
 def truc():
     while True:
@@ -21,17 +20,16 @@ def truc():
                 process_query(query)
 
 def send_to_gpt(query):
-    #response = openai.Completion.create(engine="text-davinci-003",
-    #                                    prompt=query,
-    #                                     temperature = 0.6,
-    #                                     max_tokens = 350)
-    #
-    response = llm(query, max_tokens=32, stop=["Q:", "\n"], echo=True)
+    response = openai.Completion.create(engine="text-curie-001",
+                                        prompt=query,
+                                         temperature = 0.6,
+                                         max_tokens = 350)
+    
     return response.choices[0].text
     
 
 def init_gpt():
-    #openai.api_key = API_KEY
+    openai.api_key = API_KEY
 
     print(prepromptStr)
     response = send_to_gpt(prepromptStr)
